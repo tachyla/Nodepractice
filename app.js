85d8007a503446a296bded39d2b2e423a754c991
TaChyla <tachylad.murray@gmail.com>
2021-01-25 14:34:24 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const EventEmitter = require ('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('message logged', (arg) => {
+    console.log("Logged Message", arg);
+});
+
+emitter.on('Logging', (e) => {
+    console.log('Logged Message', e)
+});
+
+// Raise an event
+emitter.emit('message logged', {id: 1, url:'http://'});
+
+// Raise logging
+emitter.emit('Logging', {data: 'logging message'});
